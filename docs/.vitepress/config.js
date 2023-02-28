d92cd6d3405a933264de2b155f4802694c7099bb
shell-Raining <shellRaining@gmail.com>
2023-02-28 13:00:09 +0800

    add new page and change actions

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,39 @@
 export default {
-  title: 'VitePress Blog',
+  title: 'ShellRaining Blog',
   description: 'Just playing around.',
   cleanUrls: true,
+  themeConfig: {
+    siteTitle: 'shellRaining Blog',
+    nav: [
+      { text: 'Github', link: 'https://github.com/shellRaining' },
+    ],
+    sidebar: [
+      {
+        text: 'vitepress',
+        collapsed: true,
+        items: [
+          { text: 'Introduction', link: '/index' },
+        ],
+      },
+      {
+        text: 'javascript',
+        collapsed: true,
+        items: [
+          { text: 'oop', link: '/javascript/oop' },
+          { text: 'reflect', link: '/javascript/reflect' },
+        ],
+      },
+      {
+        text: 'npm',
+        collapsed: true,
+        items: [
+          { text: 'index', link: '/npm/index' },
+          { text: 'package.json', link: '/npm/package_json_usage' },
+        ],
+      },
+    ],
+    editLink: {
+      pattern: 'https://github.com/shellRaining/shellRaining.github.io',
+    },
+  },
 }
---
+++ docs/vitepress/index.md
@@ -0,0 +1,3 @@
+# vitepress
+
+使用 vitepress 搭建个人博客
